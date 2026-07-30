4b8de70782c2ca9a51cfcb1a0e24a26251e1c7e2
Maytee Sukchuang <152937017+MyTxweProgrammit@users.noreply.github.com>
2026-07-30 09:14:13 +0000

    Real First Commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <title>ClassDoom - A Platform That Wil Help You To Manage Classroom</title>
+      <div className="google-sans">
+        <header className="shadow-lg w-screen h-[60px] flex justify-around items-center">
+          <p className="text-[#003EA8] font-bold text-[20px]">SuperDoom</p>
+          <div className="flex items-center gap-[20px]">
+            <div className="cursor-pointer">Login</div>
+            <div className="cursor-pointer text-white bg-[#003EA8] px-[13px] py-[7px] rounded-lg">Register</div>
+          </div>
+        </header>
+        <div className="px-[10px]">
+          <p className="mt-[40px] text-[60px]">SuperDoom</p>
+          <p>จัดการตารางเรียนและ Event ต่างๆ ในห้องเรียน</p>
+          <div className="mt-[10px] text-[#003EA8] bg-[#DBE1FF] w-fit px-[10px] py-[7px] rounded-lg">BETA</div>
+        </div>
+      </div>
+    </>
+  );
+}
