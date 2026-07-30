1d2ee551ca91ac190e36362d8d837ff4e18b486f
Maytee Sukchuang <152937017+MyTxweProgrammit@users.noreply.github.com>
2026-07-30 09:15:20 +0000

    let's go

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ export default function App() {
 
   return (
     <>
-      <title>ClassDoom - A Platform That Wil Help You To Manage Classroom</title>
+      <title>StrikeDoom - A Platform That Wil Help You To Manage Classroom</title>
       <div className="google-sans">
         <header className="shadow-lg w-screen h-[60px] flex justify-around items-center">
-          <p className="text-[#003EA8] font-bold text-[20px]">SuperDoom</p>
+          <p className="text-[#003EA8] font-bold text-[20px]">StrikeDoom</p>
           <div className="flex items-center gap-[20px]">
             <div className="cursor-pointer">Login</div>
             <div className="cursor-pointer text-white bg-[#003EA8] px-[13px] py-[7px] rounded-lg">Register</div>
           </div>
         </header>
         <div className="px-[10px]">
-          <p className="mt-[40px] text-[60px]">SuperDoom</p>
+          <p className="mt-[40px] text-[60px]">StrikeDoom</p>
           <p>จัดการตารางเรียนและ Event ต่างๆ ในห้องเรียน</p>
           <div className="mt-[10px] text-[#003EA8] bg-[#DBE1FF] w-fit px-[10px] py-[7px] rounded-lg">BETA</div>
         </div>
